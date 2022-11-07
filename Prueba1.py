5dcfbc986c11348658ff31cbb6aa960f686a526a
Debora Cuestas <91509952+Debora-Cuestas@users.noreply.github.com>
2022-11-07 15:18:50 -0300

    Add files via upload

---
+++ Prueba1.py
@@ -0,0 +1,19 @@
+nombre= 'Debora'
+print(nombre)
+edad= 21
+print(edad)
+print("Hola Mundo" + " "+ "Holaaaaaaa"+ " "+ str(edad))
+
+for x in range (5+100):
+ print("Hola Mundo!")
+
+  
+a = 18
+b = 32
+if a==b:
+  print("Es igual")
+elif a!=b:
+  print("Seguí intentando")
+else:
+  print("No se pudo obtener el resultado deseado")
+
---
+++ Prueba2.py
@@ -0,0 +1,11 @@
+semaforo_rojo= "rojo"
+semaforo_amarillo= "amarillo"
+semaforo_verde = "verde"
+color = "rojo" 
+
+if semaforo_verde == color:
+ print('Arrancar')
+elif semaforo_amarillo == color:
+ print('Espere')
+else:
+ print ('Deténgase')
